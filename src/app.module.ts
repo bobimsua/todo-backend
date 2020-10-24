2fe384583906bfebb7773a675b8494a7849d7859
Thanh Vu <thanhvd89@gmail.com>
2020-10-24 18:15:18 +0700

    Add graphql

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,15 @@
 import { Module } from '@nestjs/common';
+import { GraphQLModule } from '@nestjs/graphql';
+import { join } from 'path';
 import { WordModule } from './word/word.module';
 
 @Module({
-  imports: [WordModule],
-  controllers: [],
-  providers: [],
+  imports: [
+    GraphQLModule.forRoot({
+      autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
+      sortSchema: true,
+    }),
+    WordModule,
+  ],
 })
 export class AppModule {}
