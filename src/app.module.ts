533f839ff96fcd2ffbf9d1bdc784baace200d8a9
Thanh Vu <thanh.vu@mttjsc.com>
2020-11-18 19:22:17 +0700

    Add passport local strategy

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,13 @@
 import { Module } from '@nestjs/common';
 import { GraphQLModule } from '@nestjs/graphql';
 import { join } from 'path';
+import { AppController } from './app.controller';
 import { WordModule } from './word/word.module';
 import { AuthModule } from './auth/auth.module';
 import { UsersModule } from './users/users.module';
 
 @Module({
+  controllers: [AppController],
   imports: [
     GraphQLModule.forRoot({
       autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
