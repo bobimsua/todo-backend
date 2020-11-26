ea73ce77bbbf7bc1a5dfe86f04c3eb3bd520e5c6
Thanh Vu <thanh.vu@mttjsc.com>
2020-11-26 18:36:39 +0700

    Update jwt auth guard

--- src/auth/jwt-auth.guard.ts
+++ src/auth/jwt-auth.guard.ts
@@ -3,6 +3,9 @@ import { GqlExecutionContext } from '@nestjs/graphql';
 import { AuthGuard } from '@nestjs/passport';
 
 @Injectable()
+export class JwtAuthGuard extends AuthGuard('jwt') {}
+
+@Injectable()
 export class GqlJwtAuthGuard extends AuthGuard('jwt') {
   getRequest(context: ExecutionContext) {
     const ctx = GqlExecutionContext.create(context);
